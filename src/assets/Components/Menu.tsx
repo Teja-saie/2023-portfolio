3d56944c9b7304393fc72fb69b5d9ce5ee96fb62
Teja-Saie <tejavenkatsai8@gmail.com>
2023-03-04 13:57:05 +0530

    added styling to menu

--- src/assets/Components/Menu.tsx
+++ src/assets/Components/Menu.tsx
@@ -1,18 +1,43 @@
-import React from 'react'
-import {NavLink} from "react-router-dom"
-
-
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Menu = () => {
   return (
     <div>
-        <nav className='flex space-x-4'>
-        <NavLink to="/" className={({ isActive }) =>   isActive ? 'bg-slate-100' : undefined} >Home</NavLink>
-        <NavLink to="/resume/" className={({ isActive }) =>   isActive ? 'activeStyle' : undefined} >Resume</NavLink>
-        <NavLink to="/contact/" className={({ isActive }) =>   isActive ? 'activeStyle' : undefined} >Contact</NavLink>
-        </nav>
+      <nav className="flex space-x-4">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "bg-slate-100 rounded-lg text-orange-500 p-1"
+              : "hover:bg-red-600 hover:rounded-lg hover:text-white"
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/resume/"
+          className={({ isActive }) =>
+            isActive
+              ? "bg-slate-100 rounded-lg text-orange-500 p-1"
+              : "hover:bg-red-600 hover:rounded-lg hover:text-white"
+          }
+        >
+          Resume
+        </NavLink>
+        <NavLink
+          to="/contact/"
+          className={({ isActive }) =>
+            isActive
+              ? "bg-slate-100 rounded-lg text-orange-500 p-1"
+              : "hover:bg-red-600 hover:rounded-lg hover:text-white"
+          }
+        >
+          Contact
+        </NavLink>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
