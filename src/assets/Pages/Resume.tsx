92dc557bdd6310e9d960422baa89417a2d078264
Teja-Saie <tejavenkatsai8@gmail.com>
2023-03-05 01:24:18 +0530

    Added input with localhost node backend

--- src/assets/Pages/Resume.tsx
+++ src/assets/Pages/Resume.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+
 
 const Resume = () => {
+  const inputRef=React.useRef<null | HTMLInputElement>(null)
+  const [Data, setData] = React.useState<Blob | null>(null)
+
+  function FileUpload(e:React.ChangeEvent<HTMLInputElement>){
+    console.log(e.target.files)
+    if(e.target.files){setData(e.target.files[0]);}
+
+  }
+  function handleChange(e:React.FormEvent<HTMLFormElement>){
+    e.preventDefault();
+    let url="http://localhost:3004/data";
+    let formData = new FormData()
+    if(Data!=null && inputRef.current!=null){
+    formData.append("file", Data);
+    axios.post(url,formData,{headers: {"Content-Type": "multipart/form-data"}}).catch(err=>console.log(err))
+    setData(null);
+    inputRef.current.value="";
+  }
+  }
   return (
-    <div><h1>Resume Download Feature Goes Here</h1></div>
+    <div>
+      <h1>Resume Download Feature Goes Here</h1>
+      <form onSubmit={handleChange}>
+        <input type="file" onChange={FileUpload} accept=".jpg, .jpeg, .png,.txt,.pdf,video/*" ref={inputRef}/>
+        <input type="submit"></input>
+      </form>
+    </div>
   )
 }
 
