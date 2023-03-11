cf4a77edcd53f684707b754e3bf1a4c4db82bae5
Teja-Saie <tejavenkatsai8@gmail.com>
2023-03-12 01:55:30 +0530

    Fixed contact page and resume page styling

--- src/assets/Pages/Contact.tsx
+++ src/assets/Pages/Contact.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 export default function ContactForm() {
   const [state, handleSubmit] = useForm("mayzbwbz");
-  
+
   if (state.succeeded) {
     return (
-      <p className='text-white font-semibold text-2xl text-center'>
+      <p className="pt-[12vh] text-white font-semibold text-2xl text-center">
         Thanks for submitting!
       </p>
     );
@@ -14,95 +14,115 @@ export default function ContactForm() {
 
   return (
     <div className="pt-[12vh] flex justify-center flex-col items-center ">
-      <div className='w-4/5'>
-        <h1 className='text-white text-2xl py-2'>Contact Us</h1>
-    <p className='text-white '>Have a question, comment, or project idea? Let us know! We're passionate about building high-quality websites and web applications that help businesses and organizations succeed online.
-<br/>
-<br/>
-Our team of experienced web developers is dedicated to creating custom solutions tailored to your unique needs. Whether you're looking to launch a new website, improve your existing site's functionality, or implement a complex web application, we're here to help.
-<br/>
-<br/>
-Fill out the form below to get in touch with us, and we'll get back to you as soon as possible. We look forward to hearing from you!</p>
-    <div className='py-1'>
-      <div className='max-w-4xl mx-auto px-4 sm:px-6 lg:px-8'>
-        <div className='shadow-md rounded px-8 pt-6 pb-8 mb-4'>
-          <form onSubmit={handleSubmit} className='mx-auto max-w-md'>
-            <div className='mb-4'>
-              <label htmlFor="name" className='block text-gray-700 font-bold mb-2'>
-                Name
-              </label>
-              <input
-                id="name"
-                name="name"
-                type="text"
-                required
-                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-100'
-              />
+      <div className="w-4/5">
+        <h1 className="text-white text-2xl py-2">Contact Us</h1>
+        <p className="text-white ">
+          Have a question, comment, or project idea? Let us know! We're
+          passionate about building high-quality websites and web applications
+          that help businesses and organizations succeed online.
+          <br />
+          <br />
+          Our team of experienced web developers is dedicated to creating custom
+          solutions tailored to your unique needs. Whether you're looking to
+          launch a new website, improve your existing site's functionality, or
+          implement a complex web application, we're here to help.
+          <br />
+          <br />
+          Fill out the form below to get in touch with us, and we'll get back to
+          you as soon as possible. We look forward to hearing from you!
+        </p>
+        <div className="py-1">
+          <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="shadow-md rounded px-8 pt-6 pb-8 mb-4">
+              <form onSubmit={handleSubmit} className="mx-auto max-w-md">
+                <div className="mb-4">
+                  <label
+                    htmlFor="name"
+                    className="block text-gray-700 font-bold mb-2"
+                  >
+                    Name
+                  </label>
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    required
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-100"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="phone"
+                    className="block text-gray-700 font-bold mb-2"
+                  >
+                    Phone Number
+                  </label>
+                  <input
+                    id="phone"
+                    type="tel"
+                    maxLength={10}
+                    name="phone"
+                    required
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-100"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="email"
+                    className="block text-gray-700 font-bold mb-2"
+                  >
+                    Email Address
+                  </label>
+                  <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    required
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-100"
+                  />
+                  <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                    className="text-red-500 text-xs italic"
+                  />
+                </div>
+
+                <div className="mb-4">
+                  <label
+                    htmlFor="message"
+                    className="block text-gray-700 font-bold mb-2"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    required
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-100"
+                  />
+                  <ValidationError
+                    prefix="Message"
+                    field="message"
+                    errors={state.errors}
+                    className="text-red-500 text-xs italic"
+                  />
+                </div>
+
+                <div className="flex items-center justify-center">
+                  <button
+                    type="submit"
+                    disabled={state.submitting}
+                    className="bg-third hover:bg-fourth text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
             </div>
-            <div className='mb-4'>
-              <label htmlFor="phone" className='block text-gray-700 font-bold mb-2'>
-                Phone Number
-              </label>
-              <input
-                id="phone"
-                type="tel" 
-                maxLength={10}
-                name="phone"
-                required
-                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-100'
-              />
-            </div>
-            <div className='mb-4'>
-              <label htmlFor="email" className='block text-gray-700 font-bold mb-2'>
-                Email Address
-              </label>
-              <input
-                id="email"
-                type="email" 
-                name="email"
-                required
-                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-100'
-              />
-              <ValidationError 
-                prefix="Email" 
-                field="email"
-                errors={state.errors}
-                className='text-red-500 text-xs italic'
-              />
-            </div>
-            
-            <div className='mb-4'>
-              <label htmlFor="message" className='block text-gray-700 font-bold mb-2'>
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                required
-                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-100'
-              />
-              <ValidationError 
-                prefix="Message" 
-                field="message"
-                errors={state.errors}
-                className='text-red-500 text-xs italic'
-              />
-            </div>
-            
-            <div className='flex items-center justify-center'>
-              <button
-                type="submit"
-                disabled={state.submitting} 
-                className='bg-third hover:bg-fourth text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-              >
-                Submit
-              </button>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
-      </div>
-      </div>
-      </div>
-  )
+    </div>
+  );
 }
--- src/assets/Pages/Resume.tsx
+++ src/assets/Pages/Resume.tsx
@@ -8,11 +8,11 @@ function DownloadResume() {
     <motion.div  initial={{ opacity: 0}}
     animate={{ opacity: 1}}
     exit={{ opacity: 0}}
-    transition={{ duration: 2 }} className="pt-[10vw] h-screen flex flex-col justify-center items-center">
+    transition={{ duration: 2 }} className="pt-[10vw] h-[70vh] flex flex-col justify-center items-center w-screen">
       {/* <h1>Download My Resume</h1>
       <p>Click the button below to download my resume in PDF format.</p>
       <a href={"#"} target="_blank" download><button>Download Resume</button></a> */}
-      <h1 className='text-4xl text-white'>This Page is under construction <MdConstruction className='inline animate-bounce'/></h1>
+      <h1 className='text-4xl text-white w-[80vw]'>This Page is under construction <MdConstruction className='inline animate-bounce'/></h1>
     </motion.div>
   );
 }
