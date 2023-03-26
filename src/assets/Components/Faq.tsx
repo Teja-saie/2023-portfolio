65abaf85f59d6df33e4b1bd7f42a412592cb30cb
Teja-Saie <tejavenkatsai8@gmail.com>
2023-03-26 14:03:52 +0530

    Made CHanges to accordion and added resume

--- src/assets/Components/Faq.tsx
+++ src/assets/Components/Faq.tsx
@@ -6,16 +6,16 @@ const Faq = () => {
     <div className="flex flex-col items-center w-full">
       <div className="w-full max-w-md">
         <MyAccordion
-          title="First Accordion is very good accordion i am very happy using it now thanks "
-          content="First Accordion Content"
+          title="Why should we hire you for a MERN stack developer role?"
+          content="I believe I would be an ideal candidate for the MERN stack developer role because I have a strong background in web development and extensive experience working with the MERN stack. I have a deep understanding of React, Node.js, Express, and MongoDB and have built several successful applications using these technologies. In my previous roles, I have worked on various projects that involve designing and implementing scalable and responsive web applications, building APIs, and integrating third-party APIs. "
         />
         <MyAccordion
-          title="Second Accordion"
-          content="Second Accordion Content"
+          title="What experience do you have with the MERN stack?"
+          content="I have 2 years of experience working with the MERN stack, during which I have built various applications. My experience with each of the technologies in the MERN stack has allowed me to create scalable, efficient, and responsive applications that meet or exceed customer expectations"
         />
         <MyAccordion
-          title="Third Accordion"
-          content="Third Accordion Content"
+          title="How do you stay up to date with the latest trends and technologies in web development?"
+          content="I stay up to date with the latest trends and technologies in web development by attending conferences, participating in online forums and webinars, reading blogs, and experimenting with new tools and frameworks. I also enjoy collaborating with other developers and learning from their experiences, which has helped me stay current with industry best practices."
         />
       </div>
     </div>
--- src/assets/Pages/Resume.tsx
+++ src/assets/Pages/Resume.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 // import resumePDF from './resume.pdf';
 import { motion } from 'framer-motion';
-import {MdConstruction} from "react-icons/md"
+import {MdArrowCircleDown} from "react-icons/md"
+import { Link } from 'react-router-dom';
+import Resume from "../Resume.pdf"
 
 function DownloadResume() {
   return (
     <motion.div  initial={{ opacity: 0}}
     animate={{ opacity: 1}}
     exit={{ opacity: 0}}
-    transition={{ duration: 2 }} className="text-center pt-[10vw] h-[70vh] flex flex-col justify-center items-center w-screen">
+    transition={{ duration: 2 }} className="text-center pt-[10vw] min-h-[85vh] flex flex-col justify-center items-center w-screen">
       {/* <h1>Download My Resume</h1>
       <p>Click the button below to download my resume in PDF format.</p>
       <a href={"#"} target="_blank" download><button>Download Resume</button></a> */}
-      <h1 className='text-4xl text-white w-[80vw]'>This Page is under construction <MdConstruction className='inline animate-bounce'/></h1>
+      <h1 className='text-4xl text-white w-[80vw]'>You can download my Resume by clicking on button Below</h1>
+      <Link className='flex text-white items-center text-center border-2 border-third p-2 rounder-lg m-3 hover:bg-third hover:text-black font-semibold transition ease-in-out rounded-lg justify-center' to={Resume} download target={'_blank'}>Download Resume <MdArrowCircleDown className='ml-2 text-base'/></Link>
     </motion.div>
   );
 }
